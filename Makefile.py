76f351f4a29b509902ef0d2dade936e9a8bbf5bd
Josh <joshuacoleman@u.northwestern.edu>
2021-01-23 11:50:13 -0500

    add args to cloudbuild

--- Makefile.py
+++ Makefile.py
@@ -1,15 +1,11 @@
 setup:
-
     python3 -m venv ~/sentiment-reviews
 
 install:
-
     pip install -r requirements.txt
 
 test:
-
     PYTHONPATH=. && pytest -vv --cov=sentiment_reviews tests/*.py
 
 lint:
-
     pylint --disable=R,C sentiment_reviews
