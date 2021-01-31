954b8880e15b744fc8aebe503e1fc2c9ed775cb2
josh <jaycole7@vt.edu>
2021-01-31 22:06:58 +0000

    add hello route

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-#test
+from flask import jsonify
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello_form():
     return render_template('text_form.html')
 
+@app.route('/hello/<name>')
+def echo(name):
+    print(f"This was placed in the url: {name}")
+    val = {"Greeting": f"Hello {name}!"}
+    return jsonify(val)
 if __name__=='__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
