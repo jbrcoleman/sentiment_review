1a416645ab51df76b6e6cd1930e4b44b74102349
josh <jaycole7@vt.edu>
2021-02-01 01:36:56 +0000

    add background picture

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from flask import jsonify
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -11,5 +12,11 @@ def echo(name):
     print(f"This was placed in the url: {name}")
     val = {"Greeting": f"Hello {name}!"}
     return jsonify(val)
+
+@app.route('/json/<name>')
+def test(name):
+    return json.dumps(f"Hi {name}!")
+
+
 if __name__=='__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
