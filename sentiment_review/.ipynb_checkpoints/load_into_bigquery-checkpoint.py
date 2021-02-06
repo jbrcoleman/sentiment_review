ae744e74aac26a2a5008e2b753e68bfbd673ede2
jc <jaycole7@vt.edu>
2021-02-06 00:19:20 +0000

    add load to bigquery script

---
+++ sentiment_review/.ipynb_checkpoints/load_into_bigquery-checkpoint.py
@@ -0,0 +1,29 @@
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+# TODO(developer): Set table_id to the ID of the table to create.
+table_id = "reviews.reviews"
+
+job_config = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("stars", "INTEGER"),
+        bigquery.SchemaField("text", "STRING"),
+    ],
+    allow_quoted_newlines= True,
+    ignore_unknown_values=True,
+    max_bad_records=10,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+uri = "gs://reviews_yelp/yelp_reviews.csv"
+
+load_job = client.load_table_from_uri(
+    uri, table_id, job_config=job_config
+)  # Make an API request.
+
+load_job.result()  # Waits for the job to complete.
+
+destination_table = client.get_table(table_id)  # Make an API request.
+print("Loaded {} rows.".format(destination_table.num_rows))
---
+++ sentiment_review/upload_csv.py
@@ -0,0 +1,10 @@
+from google.cloud import storage
+
+#instantiate client
+client=storage.Client()
+# Creating bucket object
+bucket = client.get_bucket('reviews_yelp')
+# Name of the object to be stored in the bucket
+object_name_in_gcs_bucket = bucket.blob('yelp_reviews.csv')
+# Name of the object in local file system
+object_name_in_gcs_bucket.upload_from_filename('../data/yelp_reviews.csv')
