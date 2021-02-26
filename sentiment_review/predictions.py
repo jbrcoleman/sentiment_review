4a7b98dacb1662c86e7adf8e6a28fd267d3f2d15
jc <jaycole7@vt.edu>
2021-02-26 02:09:18 +0000

    add predictions script

---
+++ sentiment_review/predictions.py
@@ -0,0 +1,26 @@
+from google.cloud import automl
+
+# TODO(developer): Uncomment and set the following variables
+project_id = "dynamic-aurora-302220"
+model_id = "TST8315309572730388480"
+content = "This place was terrible! Stay away!!!"
+
+prediction_client = automl.PredictionServiceClient()
+
+# Get the full path of the model.
+model_full_id = automl.AutoMlClient.model_path(project_id, "us-central1", model_id)
+
+# Supported mime_types: 'text/plain', 'text/html'
+# https://cloud.google.com/automl/docs/reference/rpc/google.cloud.automl.v1#textsnippet
+text_snippet = automl.TextSnippet(content=content, mime_type="text/plain")
+payload = automl.ExamplePayload(text_snippet=text_snippet)
+
+response = prediction_client.predict(name=model_full_id, payload=payload)
+
+for annotation_payload in response.payload:
+    print("Predicted class name: {}".format(annotation_payload.display_name))
+    print(
+        "Predicted sentiment score: {}".format(
+            annotation_payload.text_sentiment.sentiment
+        )
+    )
